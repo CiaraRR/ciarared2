5351c548409c528d4c171ed2282ffbc72541b5ce
CiaraRR <160429754+CiaraRR@users.noreply.github.com>
2026-04-29 16:03:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("year").textContent = new Date().getFullYear();
-
-  new Swiper(".heroSwiper", {
-    loop: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".heroSwiper .swiper-pagination",
-      clickable: true,
-    },
-  });
+  const year = document.getElementById("year");
+  if (year) {
+    year.textContent = new Date().getFullYear();
+  }
 
   new Swiper(".workshopSwiper", {
     loop: true,
     slidesPerView: 1,
-    spaceBetween: 20,
-    observer: true,
-    observeParents: true,
+    spaceBetween: 0,
 
     navigation: {
       nextEl: ".workshopSwiper .swiper-button-next",
